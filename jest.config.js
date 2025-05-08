48bee64cb05c823451fb8a655255b7516d7b82c7
ronydevdesgn <rodivanio360@gmail.com>
2025-05-08 10:11:01 +0100

    feat: configutação do arquivo de testes da API

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,16 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
+
+/**
+* Configuração do Jest para testar projetos TypeScript/Node.js
+*
+* @description Configura o Jest para usar o ambiente de teste Node.js e ts-jest para transformação TypeScript
+* @see https://jestjs.io/docs/configuration
+* @see https://www.npmjs.com/package/ts-jest
+*/
 export default {
   testEnvironment: "node",
   transform: {
     "^.+\.tsx?$": ["ts-jest",{}],
   },
+  testMatch: ['**/src/tests/**/*.test.ts'],
 };
