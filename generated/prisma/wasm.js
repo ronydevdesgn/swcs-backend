fe9b6f0534302d9bac637f4883a42fa198fdfbd5
ronydevdesgn <rodivanio360@gmail.com>
2026-05-27 18:46:20 +0100

    chore: add prisma migration and generated files

--- generated/prisma/wasm.js
+++ generated/prisma/wasm.js
@@ -274,7 +274,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../.env"
   },
   "relativePath": "../../prisma",
